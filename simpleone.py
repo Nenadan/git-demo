e0daf152329776adf99baa1e0c661154a674f9f6
Nenadan <ristivojevicnenad@gmail.com>
2024-01-18 13:47:03 +0100

    Add simpleone.py file

---
+++ simpleone.py
@@ -0,0 +1,16 @@
+'''
+This is a simple script
+'''
+
+def my_method():
+    '''
+    This is a simple function
+    '''
+    first = 1
+    second = 2
+
+    print(first)
+    print(second)
+
+my_method()
+
